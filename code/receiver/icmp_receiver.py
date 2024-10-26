291c25d1720b3c4901913b16af64d872a51bae11
fatih-demir <fatih.demir@globetech.io>
2024-10-26 21:18:39 +0300

    init

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,3 +1,13 @@
-import scapy
+from scapy.all import sniff, ICMP
 
-# Implement your ICMP receiver here
+def handle_packet(packet):
+    if packet.haslayer(ICMP) and packet[ICMP].type == 8:
+        print("ICMP Request received:")
+        packet.show()
+
+def receive_icmp():
+    print("Listening for ICMP packets...")
+    sniff(filter="icmp", prn=handle_packet)
+
+if __name__ == "__main__":
+    receive_icmp()
--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,3 +1,11 @@
-import scapy
+from scapy.all import IP, ICMP, send
 
-# Implement your ICMP sender here
+def send_icmp():
+    packet = IP(dst="172.22.0.2", ttl=1) / ICMP()
+    
+    print("Sending ICMP packet with TTL=1 to receiver...")
+    send(packet)
+    print("Packet sent.")
+
+if __name__ == "__main__":
+    send_icmp()
