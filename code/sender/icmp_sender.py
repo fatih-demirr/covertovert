70daeffbc776a86e4cffa9ff466a292d8fdda398
fatih-demirr <fatih4624demir@gmail.com>
2024-11-02 17:03:43 +0300

    latest

--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,11 +1,8 @@
 from scapy.all import IP, ICMP, send
 
 def send_icmp():
-    packet = IP(dst="172.22.0.2", ttl=1) / ICMP()
-    
-    print("Sending ICMP packet with TTL=1 to receiver...")
+    packet = IP(dst="172.22.0.2", ttl=1) / ICMP()    
     send(packet)
-    print("Packet sent.")
 
 if __name__ == "__main__":
     send_icmp()
